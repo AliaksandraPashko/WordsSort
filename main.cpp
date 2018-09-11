3103f2e5d99747fc9c66e95836f9c827c30584cc
AliaksandraPashko <aliaksandra.pashko@solarwinds.com>
2018-09-11 23:18:04 +0300

    create a function, add boost sort

--- main.cpp
+++ main.cpp
@@ -3,33 +3,45 @@
 #include <sstream>
 #include <string>
 #include <vector>
-#include <iterator>
 
-int main() {
-    std::ifstream fin("input.txt");
-    std::ofstream fout("output.txt");
-    std::string wordToErase;
+#include <boost/algorithm/string.hpp>
 
+void wordsSort(std::ifstream &fin, std::ofstream &fout, const std::string &wordToErase)
+{
     std::vector<std::string> words;
-
-    std::cin >> wordToErase;
 
     std::string currentString;
 
     while (std::getline(fin, currentString)) {
-        std::istringstream iss(currentString);
-        std::copy(std::istream_iterator<std::string>(iss), std::istream_iterator<std::string>(),
+
+        std::istringstream istringStream(currentString);
+
+        std::copy(std::istream_iterator<std::string>(istringStream), std::istream_iterator<std::string>(),
                   std::back_inserter(words));
+
         words.erase(std::remove(words.begin(), words.end(), wordToErase), words.end());
+
         std::sort(words.begin(), words.end(), [](const std::string& a, const std::string& b )
         {
-
+            return boost::algorithm::ilexicographical_compare(a, b);
         });
+
         std::copy(words.begin(), words.end(), std::ostream_iterator<std::string>(fout, " "));
         fout << std::endl;
-
     }
+}
 
+int main() {
+    std::ifstream fin("input.txt");
+    std::ofstream fout("output.txt");
+
+    std::string wordToErase;
+    std::cin >> wordToErase;
+
+    wordsSort(fin, fout, wordToErase);
+
+    fin.close();
     fout.close();
+
     return 0;
 }
