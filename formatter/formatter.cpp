bdc5684d76eb285ce289a4031e521c6af3e10813
AliaksandraPashko <aliaksandra.pashko@solarwinds.com>
2018-09-13 00:08:00 +0300

    create Formatter class

---
+++ formatter/formatter.cpp
@@ -0,0 +1,49 @@
+#include "formatter.h"
+
+#include <string>
+#include <sstream>
+#include <vector>
+
+#include <boost/algorithm/string.hpp>
+
+Formatter :: Formatter(const std::string& inputFile, const std::string& outputFile, const std::string& wordToErase)
+: fin_(inputFile), fout_(outputFile), wordToErase_(wordToErase) {}
+
+
+void Formatter :: sort()
+{
+    std::vector<std::string> words;
+
+    std::string currentString;
+
+    if (fin_)
+    {
+        while (std::getline(fin_, currentString)) {
+
+            std::istringstream istringStream(currentString);
+
+            std::copy(std::istream_iterator<std::string>(istringStream), std::istream_iterator<std::string>(),
+                      std::back_inserter(words));
+
+            assert(words.empty() == false);
+
+            words.erase(std::remove(words.begin(), words.end(), wordToErase_), words.end());
+
+            std::sort(words.begin(), words.end(), [](const std::string& str1, const std::string& str2)
+            {
+                return boost::algorithm::ilexicographical_compare(str1, str2);
+            });
+
+            std::copy(words.begin(), words.end(), std::ostream_iterator<std::string>(fout_, " "));
+
+            fout_ << std::endl;
+        }
+    }
+}
+
+
+Formatter::~Formatter()
+{
+    fin_.close();
+    fout_.close();
+}
---
+++ formatter/formatter.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <fstream>
+
+class Formatter
+{
+    std::ifstream fin_;
+    std::ofstream fout_;
+    std::string wordToErase_;
+
+public:
+    Formatter(const std::string&, const std::string&, const std::string& );
+    void sort();
+    ~Formatter();
+};
+
